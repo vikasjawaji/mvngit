20467b5c9b50c04c2c65341c9bb8bb06fae0af25
vk399-github <vikasjawaji007@gmail.com>
2021-05-27 16:47:30 +0530

    This version has a method to print Hello world

--- src/main/java/com/hcl/gitmvn/main/Main.java
+++ src/main/java/com/hcl/gitmvn/main/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello World");
+		greet();
+	}
 
+	private static void greet() {
+		System.out.println("Hello world");
 	}
 
 }
